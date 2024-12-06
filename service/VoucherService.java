66521c80e063a16e806defa71861a460965168e7
2381049 <2381049@unai.edu>
2024-12-06 17:26:08 +0700

    Delete service directory
    Menghapus Package yang tidak terpakai

--- service/VoucherService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public class VoucherService {
-    private static final String VALID_VOUCHER_CODE = "MTJ";
-
-    public boolean applyVoucher(String voucherCode) {
-        return VALID_VOUCHER_CODE.equalsIgnoreCase(voucherCode);
-    }
-}
